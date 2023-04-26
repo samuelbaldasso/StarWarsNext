60a3ddd8d8d0dd657ad04619e455aa4201dd39b9
Samuel Baldasso <samuelbaldasso93@gmail.com>
2023-04-26 16:26:50 -0300

    first commit

---
+++ pages/Home/style.ts
@@ -0,0 +1,31 @@
+
+import styled from "styled-components";
+
+export const H1 = styled.a`
+  color: white;
+  font-size: 18px;
+`
+
+export const StarWarsLogo = styled.img`
+`
+
+export const Container = styled.div`
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+`
+
+export const Container2 = styled.div`
+display: flex;
+margin: 24px;
+justify-content: space-between;
+align-items: center;
+`
+
+export const Button = styled.button`
+  margin: 12px;
+  width: 100px;
+  border: none;
+  border-radius: 5px;
+`
---
+++ pages/[id]/style.ts
@@ -0,0 +1,38 @@
+
+import styled from "styled-components";
+
+export const H1 = styled.a`
+  color: #000;
+  font-size: 18px;
+  line-height: 1.5;
+  text-decoration: none;
+`
+
+export const StarWarsLogo = styled.img`
+`
+
+export const Container = styled.div`
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+`
+
+export const Container2 = styled.div`
+display: flex;
+margin: 24px;
+justify-content: space-between;
+flex-direction: column;
+background-color: #fff;
+padding: 48px;
+margin: 48px;
+border-radius: 25px;
+align-items: center;
+`
+
+export const Button = styled.button`
+  margin: 12px;
+  width: 100px;
+  border: none;
+  border-radius: 5px;
+`
